1c9d84d2a17e1a62427449b03a2ec71502c4655c
Jonathan Nolasco <jnolascob@gmail.com>
2018-05-03 18:18:35 -0500

    No olvidar el naming convention

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const cuscontroler = require('../controllers/controllerUser');
-const loginController = require('../controllers/controllerlogin');
+const userController = require('../controllers/userController');
+const loginController = require('../controllers/loginController');
 
 router.get('/login', (req, res, next) => { res.render('login'); });
 router.get('/', (req, res, next) => { res.render('login'); });
 router.get('/usuarioedit', (req, res, next) => { res.render('usuarioedit'); });
 router.post('/login', loginController.validarUsuario);
-router.get('/tablausuarios/',cuscontroler.tablauser);
-router.post('/tablausuarios/',cuscontroler.tablainsert);
-router.get('/delete/:id', cuscontroler.delete);
-router.get('/update/:id', cuscontroler.consultaedit);
-router.post('/update/:id', cuscontroler.refrescar);
+router.get('/tablausuarios/',userController.tablauser);
+router.post('/tablausuarios/',userController.tablainsert);
+router.get('/delete/:id', userController.delete);
+router.get('/update/:id', userController.consultaedit);
+router.post('/update/:id', userController.refrescar);
 
 module.exports=router;
