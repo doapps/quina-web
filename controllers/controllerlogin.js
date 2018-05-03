5e92cdef86c71ba7ba58a90e0327869fda384160
arthur2021 <arturogilserpa18@gmail.com>
2018-05-03 12:21:48 -0500

    Errores solucionados

--- controllers/controllerlogin.js
+++ controllers/controllerlogin.js
@@ -7,19 +7,17 @@ user.validarUsuario = (req, res) => {
     const encryptPassword = require('crypto').createHash('md5').update(passwords).digest('hex');
 
    
-    db.query('select correo,contra from usuario where correo= ? or contra=? ',[email,encryptPassword], function (err, result, fields){
+    db.query('select correo,contra from usuario where correo=? and contra=?',[email,encryptPassword], function (err, result, fields){
         var vercorreo=false;
         var vercontra=false;
-        
         if(result.length==0) {
             vercorreo=false;
              vercontra=false;
             
         }else{
-
         var rcor=result[0].correo == email;
         var rcon=result[0].contra == encryptPassword;
-    
+       
 
             if(rcor==true){
                 vercorreo=true;
@@ -29,9 +27,8 @@ user.validarUsuario = (req, res) => {
             }
 
         }
-
-
-      
+       
+       
       res.render("login",{"vercorreo":vercorreo,"vercontra":vercontra});
     });
 };
