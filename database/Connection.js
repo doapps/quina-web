11fab7041472968fd52496ddb9a6d121750cbf62
Jonathan Nolasco <jnolascob@gmail.com>
2018-05-09 21:48:27 -0500

    Corregir espaciados y nomenclatura en Connection.ks

--- database/Connection.js
+++ database/Connection.js
@@ -4,20 +4,21 @@ require('dotenv').config();
 let conn;
 
 function connect() {
-    if (!conn) {
-        conn = mysql.createConnection({
-            host     : process.env.DB_HOST,
-            user     : process.env.DB_USERNAME,
-            password : process.env.DB_PASSWORD,
-            database : process.env.DB_DATABASE,
-            multipleStatements: true
-          });
-        conn.connect((err) => {
-            if (err)
-                console.log('error al conectar ' + err);
-        });
-    }  
-    return conn;
+  if (!conn) {
+    conn = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      multipleStatements: true,
+    });
+    conn.connect((err) => {
+      if (err) {
+        console.log(`error al conectar: ${err}`);
+      }
+    });
+  }
+  return conn;
 }
 
 module.exports = connect();
